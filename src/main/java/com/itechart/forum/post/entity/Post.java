3c3676d5f1bf250f0144fe3b7db11d3c75d9b81e
liliya <liliaqwer@gmail.com>
2019-09-30 17:00:37 +0300

    added entity, service for comment

--- src/main/java/com/itechart/forum/post/entity/Post.java
+++ src/main/java/com/itechart/forum/post/entity/Post.java
@@ -1,40 +1,43 @@
 package com.itechart.forum.post.entity;
 
+import com.itechart.forum.comment.Comment;
 import com.itechart.forum.post.type.CategoryType;
 import com.itechart.forum.user.entity.User;
+import lombok.Data;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.time.LocalDate;
+import java.util.List;
 
 @Entity
 @Table(name = "post")
+@EntityListeners(AuditingEntityListener.class)
 @SecondaryTable(name = "post_content", pkJoinColumns = @PrimaryKeyJoinColumn(name = "fk_post_id", referencedColumnName = "id"))
+@Data
 public class Post {
     @Id
     @GeneratedValue(generator = "post_id_generator")
     @SequenceGenerator(name = "post_id_generator", sequenceName = "post_id_seq", allocationSize = 1)
     private Integer id;
 
-    @NotNull(message = "title.notnull")
-    @Size(max = 50, message = "{title.maxsize}")
     private String title;
 
     @Enumerated(EnumType.ORDINAL)
-    @NotNull(message = "category.notnull")
     private CategoryType category;
 
-    @NotNull(message = "title.notnull")
-    @Size(max = 100, message = "{description.maxsize}")
     private String description;
 
     @Basic(fetch = FetchType.LAZY)
-    @NotNull(message = "body.notnull")
     private String body;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "post")
+    private List<Comment> commentList;
 
     @Column(nullable = false, updatable = false)
     @CreatedDate
